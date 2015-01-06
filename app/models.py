f1486c300ee71ef56f0068862049967129e08703
Philip Lewis <philip.lewis71239@gmail.com>
2015-01-06 12:34:29 -0500

    updating models

--- app/models.py
+++ app/models.py
@@ -2,27 +2,23 @@ import sqlite3 as sql
 from app import app
 from app import db
 from datetime import datetime
-# class Logs(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     ip_address = db.Column(db.String(40))
-#     timestamp = db.Column(db.String(120))
-
 
 class Logs(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
+	ip_addr = db.Column(db.String)
 	timestamp = db.Column(db.DateTime, default=datetime.now())
 
 
-def insert_log(ip_addr):
-    with sql.connect("database.db") as con:
-        cur = con.cursor()
-        timestamp = str(datetime.now())
-        cur.execute("INSERT into logs (ip_address,timestamp) VALUES (?,?)", (ip_addr, timestamp))
-        con.commit()
+# def insert_log(ip_addr):
+#     with sql.connect("database.db") as con:
+#         cur = con.cursor()
+#         timestamp = str(datetime.now())
+#         cur.execute("INSERT into logs (ip_address,timestamp) VALUES (?,?)", (ip_addr, timestamp))
+#         con.commit()
 
-def get_logs():
-    with sql.connect("database.db") as con:
-        cur = con.cursor()
-        result = cur.execute("select * from logs").fetchall()
-    return result
+# def get_logs():
+#     with sql.connect("database.db") as con:
+#         cur = con.cursor()
+#         result = cur.execute("select * from logs").fetchall()
+#     return result
 
